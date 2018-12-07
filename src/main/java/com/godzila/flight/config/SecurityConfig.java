482dbceed7fc21265aa72126d668417cd87597ec
SN-patel <snpatel.jsg@gmail.com>
2018-12-07 16:46:15 +0530

    commit-22

--- src/main/java/com/godzila/flight/config/SecurityConfig.java
+++ src/main/java/com/godzila/flight/config/SecurityConfig.java
@@ -1,7 +1,22 @@
 package com.godzila.flight.config;
 
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
+	
+	@Override
+    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+          .withUser("satya").password("password");
+    }
+ 
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+        http
+          .authorizeRequests()
+          .antMatchers("/api*").permitAll();
+    }
 }
